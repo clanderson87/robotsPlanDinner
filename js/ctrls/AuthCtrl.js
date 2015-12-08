a03793d850fe38c4a6c2ab7b630f0492d6a374c9
Chris Anderson <chrisanderson@Chriss-MacBook-Pro.local>
2015-12-07 21:07:55 -0600

    Got google authentication and writing to firebase
    still need calendar scope

--- js/ctrls/AuthCtrl.js
+++ js/ctrls/AuthCtrl.js
@@ -5,48 +5,26 @@ app.controller('AuthCtrl', ["$firebaseAuth", "$location", "$firebaseObject",
 
 //Add empty currentUser
   var currentUser = null;
-  var username = "";
 
 //init the firebase
   var ref = new Firebase("https://rpd.firebaseio.com");
+  var usersRef = ref.child('users');
 
 //c&p from firebase, wrapped in function
   this.login = function(){
-    ref.authWithOAuthPopup("google", function(error, authData) {
-      if (error) {
-       console.log("Login Failed!", error);
-      } else {
-        console.log("Authenticated successfully with payload:", authData)
-        username = authData.google.displayName;
-        console.log(username);
-        // $location.path("/pref1");
-        var userRef = new Firebase("https://rpd.firebaseio.com/users/" + authData.uid);
-        var userRefObj = $firebaseObject(userRef);
+      ref.authWithOAuthPopup("google", function(error, authData) {
+        if (error) {
+          console.log("Login Failed!", error);
+        } else {
+          console.log("Authenticated successfully with payload:", authData);
+          userRefObj = authData;
+          userRefObj.username = authData.google.displayName;
+          ref.child("users").child(authData.uid).set(userRefObj);
 
-                //promise that is resolved once the data is available from firebase
-        userRefObj.$loaded()
-          .then(function(){
-
-            currentUser = userRefObj;
-            currentUser.displayName = username;
-            console.log(currentUser);
-            console.log("currentUser set as ", currentUser);
-          });
-
-        $location.path("/pref1");
-
+        };
       }
-        //Fires this code:
-
-              //end promise
-
-              //current user set to the auth data
-                // end set user}
-      {
-      scope: "https://www.googleapis.com/auth/calendar"
-    }
-  });
-}
+  )
+};
 
 //end Controller Function
 }]);
