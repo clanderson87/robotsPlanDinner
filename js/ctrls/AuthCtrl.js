e8c6fa28e816a73661e44c9fe5e34576f98a8b1b
Chris Anderson <chrisanderson@Chriss-MacBook-Pro.local>
2015-12-07 19:48:29 -0600

    Added displayName to currentUser object

--- js/ctrls/AuthCtrl.js
+++ js/ctrls/AuthCtrl.js
@@ -3,6 +3,10 @@ app.controller('AuthCtrl', ["$firebaseAuth", "$location", "$firebaseObject",
 
 //add login factory back?
 
+//Add empty currentUser
+  var currentUser = null;
+  var username = "";
+
 //init the firebase
   var ref = new Firebase("https://rpd.firebaseio.com");
 
@@ -12,9 +16,35 @@ app.controller('AuthCtrl', ["$firebaseAuth", "$location", "$firebaseObject",
       if (error) {
        console.log("Login Failed!", error);
       } else {
-        console.log("Authenticated successfully with payload:", authData);
+        console.log("Authenticated successfully with payload:", authData)
+        username = authData.google.displayName;
+        console.log(username);
+        // $location.path("/pref1");
+        var userRef = new Firebase("https://rpd.firebaseio.com/users/" + authData.uid);
+        var userRefObj = $firebaseObject(userRef);
+
+                //promise that is resolved once the data is available from firebase
+        userRefObj.$loaded()
+          .then(function(){
+
+            currentUser = userRefObj;
+            currentUser.displayName = username;
+            console.log(currentUser);
+            console.log("currentUser set as ", currentUser);
+          });
+
         $location.path("/pref1");
+
       }
+        //Fires this code:
+
+              //end promise
+
+              //current user set to the auth data
+                // end set user}
+      {
+      scope: "https://www.googleapis.com/auth/calendar"
+    }
   });
 }
 
