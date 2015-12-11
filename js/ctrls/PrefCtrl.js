e1706f76e2bdab1bf4475eaaa320d62dbb3ce3a5
Chris Anderson <chrisanderson@Chriss-MacBook-Pro.local>
2015-12-10 18:37:46 -0600

    working allergy collection

--- js/ctrls/PrefCtrl.js
+++ js/ctrls/PrefCtrl.js
@@ -12,21 +12,37 @@ app.controller('PrefCtrl',
         var user = auth.uid;
 
         var possibleLikes = ["Italian", "American", "Mexican", "German", "Chinese"];
+        var possibleAllergies = ["Peanut", "Shellfish", "Gluten"]
+        var userAllergies =[];
 
         this.possibleLikes = possibleLikes;
+        this.possibleAllergies = possibleAllergies;
+        this.userAllergies = userAllergies;
 //try pushing the values of checkboxes within the partial?
 
         this.removeLikes = function(index) {
           this.possibleLikes.splice(index, 1);
-
         };
 
-        this.addToUser = function(){
+        this.pushAllergies = function(index) {
+          this.userAllergies.push(possibleAllergies.slice(index, (index+1)).toString())
+        };
+
+        this.addLikesToUser = function(){
           console.log(user);
           for (var i = possibleLikes.length - 1; i >= 0; i--) {
             ref.child("likes").child(possibleLikes[i]).set(user);
-          };;
           };
+          $location.path('/pref2');
+          };
+
+        this.addAllergiesToUser = function(){
+          for (var i = userAllergies.length - 1; i >= 0; i--) {
+            ref.child("allergies").child(userAllergies[i]).set(user);
+          };
+          $location.path('/pref3');
+
+        }
       }
   ]
 )
